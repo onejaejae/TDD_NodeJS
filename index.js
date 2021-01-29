3a5a0fda2bbebd64c1114a46568f3451a00e7ca2
Cho Won Jae <yoteamo7@naver.com>
2021-01-30 00:59:46 +0900

    TDD로 API서버 개발(GET, DELETE)

--- index.js
+++ index.js
@@ -4,14 +4,59 @@ const app = express();
 
 app.use(morgan("dev"));
 
-const users = [
+let users = [
   { id: 1, name: "elice" },
   { id: 2, name: "bek" },
   { id: 3, name: "chris" },
 ];
 
+app.get("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  if (Number.isNaN(id)) {
+    return res.status(400).end();
+  }
+
+  const user = users.map((value) => {
+    if (value.id === id) {
+      return value;
+    }
+  })[0];
+
+  if (user) {
+    return res.json(user);
+  } else {
+    return res.status(404).end();
+  }
+});
+
 app.get("/users", (req, res) => {
-  res.json(users);
+  req.query.limits = req.query.limits || 10;
+  const limit = parseInt(req.query.limits, 10); // req.query는 문자열로 들어오르모 int형으로 전환, 10은 10진법을 의미
+
+  if (Number.isNaN(limit)) {
+    return res.status(400).end();
+  }
+  return res.json(users.slice(0, limit));
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  console.log("id", id, "req.params", req.params.id);
+
+  if (Number.isNaN(id)) {
+    res.status(400).end();
+  }
+
+  users = users.map((value) => {
+    if (value.id !== id) {
+      return value;
+    }
+  });
+
+  console.log("users", users);
+
+  res.status(204).end();
 });
 
 app.listen(3000, function () {
--- index.spec.js
+++ index.spec.js
@@ -1,13 +1,91 @@
-const { app } = require("./index");
+const e = require("express");
+const should = require("should");
 const request = require("supertest");
+const { app } = require("./index");
 
 describe("GET /users는", () => {
-  it("...", (done) => {
-    request(app)
-      .get("/users")
-      .end((err, res) => {
-        console.log(res.body);
-        done();
-      });
+  describe("성공시", () => {
+    it("유저 객체를 담은 배열로 응답한다", (done) => {
+      request(app)
+        .get("/users")
+        .end((err, res) => {
+          res.body.should.be.instanceOf(Array);
+          done();
+        });
+    });
+    it("최대 limit 개수 만큼 응답한다", (done) => {
+      request(app)
+        .get("/users?limits=2")
+        .end((err, res) => {
+          res.body.should.have.length(2);
+          done();
+        });
+    });
+  });
+
+  describe("실패시", () => {
+    it("limit이 숫자형이 아닐 경우 400을 응답한다", (done) => {
+      request(app).get("/users?limits=two").expect(400).end(done);
+    });
+  });
+});
+
+describe("GET /users/:id는", () => {
+  describe("성공시", () => {
+    it("id가 1인 유저 객체를 반환한다", (done) => {
+      request(app)
+        .get("/users/1")
+        .end((err, res) => {
+          res.body.should.have.property("id", 1);
+          done();
+        });
+    });
+  });
+  describe("실패 시", () => {
+    it("id가 숫자가 아닐 경우 400으로 응답한다", (done) => {
+      request(app).get("/users/one").expect(400).end(done);
+    });
+
+    it("id로 유저를 찾을 수 없을 경우 404로 응답한다", (done) => {
+      request(app).get("/users/5").expect(404).end(done);
+    });
+  });
+});
+
+describe("DELETE /users/:id", () => {
+  describe("성공시", () => {
+    it("204를 응답한다", (done) => {
+      request(app).delete("/users/1").expect(204).end(done);
+    });
+  });
+  describe("실패시", () => {
+    it("id가 숫자가 아닐 경우 400을 응답한다", (done) => {
+      request(app).delete("/users/one").expect(400).end(done);
+    });
+  });
+});
+
+describe("POST /users", () => {
+  describe("성공 시", () => {
+    let name = "daniel";
+    let body;
+    before((done) => {
+      request(app)
+        .post("/users")
+        .send({ name })
+        .expect(201)
+        .end((err, res) => {
+          body = res.body;
+          done();
+        });
+    });
+
+    it("생성된 유저 객체를 반환한다", () => {
+      body.should.have.property("id");
+    });
+
+    it("입력한 name를 반환한다", () => {
+      body.should.have.property("name", name);
+    });
   });
 });
