3977e8870432ca9bdf40d7335d06829a5677e771
Cho Won Jae <yoteamo7@naver.com>
2021-01-29 16:30:44 +0900

    슈드 + 슈퍼테스 학습

--- index.js
+++ index.js
@@ -17,3 +17,5 @@ app.get("/users", (req, res) => {
 app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
 });
+
+module.exports = { app };
--- utils.spec.js
+++
@@ -1,9 +0,0 @@
-const { capitalize } = require("./utils");
-const assert = require("assert");
-
-describe("utils.js 모듈의 capitalize 함수는", () => {
-  it("문자열의 첫번째 문자를 대문자로 넘겨준다", () => {
-    const result = capitalize("hello");
-    assert.strictEqual(result, "Hello");
-  });
-});
